a5ed1664591bcb8c3080a6747efd5b5ceca18c18
AlexOLary <AlexOLary@gmail.com>
2021-08-22 01:25:24 +0700

    Posts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, {useState} from "react";
-import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
 
 function App() {
-    const [value, setValue] = useState('Текст в ИНПУТЕ')
-
-
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'}
+    ])
 
     return (
         <div className="App">
-            <Counter/>
+            {posts.map(post =>
+            <PostItem post={post}/>
+            )}
         </div>
     );
 }
